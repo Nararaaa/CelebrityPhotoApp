032c84cf4fc315f2c4a8a2fc92037380b70e8990
DESKTOP-4LFJF15\rnjss <rnjsskfk123@naver.com>
2022-11-17 15:54:02 +0900

    Toast 메세지를 통해 간단히 버튼클릭이벤트 확인

--- app/src/main/java/com/example/celebrityphotoapp/MainActivity.kt
+++ app/src/main/java/com/example/celebrityphotoapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.celebrityphotoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        // 화면을 보여주는 코드
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 버튼 클릭 이벤트 코드
+        val image1 = findViewById<ImageView>(R.id.bts_image_1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
